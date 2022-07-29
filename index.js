982cb26799001613389f04aa64c637238f5b5a23
Zemyoro <85952603+Zemyoro@users.noreply.github.com>
2022-07-29 15:25:26 +0900

    .

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 import { execFile } from 'child_process';
 import dwebp from 'dwebp-bin';
 import fs from 'fs';
-convert();
 
-function convert() {
-    const path = process.argv[2];
+// Examples
+// 1. ./images (This project inside images folder)
+// 2. ../images (Directory above this project inside images folder)
+// 3. ../../images (Directory 2 folders above this project inside images folder)
+// And it goes on...
 
-    if (!path || !path.includes('.') || !path.endsWith('/'))
-        return console.log('Please provide a path to the folder containing the webp files! (e.g. "node index.js ../" or "node . ../" or "node . ../emotes")');
+if (!process.argv[2].endsWith('/'))
+    process.argv[2] += '/';
+const path = process.argv[2];
 
-    const files = fs.readdirSync(`${path}`).filter(file => file.endsWith('.webp'));
-    if (!files.length) return console.log('No files to convert');
+const files = fs.readdirSync(`${path}`).filter(file => file.endsWith('.webp'));
+if (!files.length) {
+    console.log('No files to convert');
+    process.exit(1);
+}
 
-    for (const file of files) {
-        execFile(dwebp, [`${path}${file}`, '-o', `./images/${file.replace('.webp', '.png')}`], error => {
-            if (error) throw error;
+for (const file of files) {
+    execFile(dwebp, [`${path}${file}`, '-o', `./images/${file.replace('.webp', '.png')}`], error => {
+        if (error) throw error;
 
-            console.log(`Converted ${file} to png`)
-        });
-    }
+        console.log(`Converted ${file} to png`)
+    });
 }
